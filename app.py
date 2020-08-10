c26254002e47fabbd94a4798eb29e3e27b7cf1db
Geoff <gnel.syncronetics@gmail.com>
2020-08-10 16:12:17 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,14 @@
-#!/usr/bin/env python
-# coding: utf-8
-# <h1>Table of Contents<span class="tocSkip"></span></h1>
-# <div class="toc"><ul class="toc-item"></ul></div>
-
 # import main libs
-import os 
 import pandas as pd
-import numpy as np
-from pandas import DataFrame as DFM
-import warnings
-warnings.filterwarnings('ignore')
-
 import plotly.express as px
-import plotly.graph_objects as go
-from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
-import plotly as py
-init_notebook_mode(connected=True)
 
-from jupyter_dash import JupyterDash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output# Load Data
 
-
 #fileurl = 'https://github.com/Printalect/Property-Listings-Dashboard/blob/master/property-listings-downsampled.csv'
 fileurlraw = 'https://raw.githubusercontent.com/Printalect/Property-Listings-Dashboard/master/property-listings-downsampled.csv'
 rawdata = pd.read_csv(fileurlraw)
-
-
-
 
 # Dashboard one
 plotdata = rawdata.groupby(['state', 'type'])['price'].mean().reset_index()
@@ -36,7 +16,10 @@ plotdata = rawdata.groupby(['state', 'type'])['price'].mean().reset_index()
 typesconsidered = ['house', 'townhouse', 'apartment']
 
 external_stylesheets1 = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = JupyterDash(__name__, external_stylesheets=external_stylesheets1)
+#from jupyter_dash import JupyterDash
+# app = JupyterDash(__name__, external_stylesheets=external_stylesheets1)
+app =  dash.Dash(__name__, external_stylesheets=external_stylesheets1)
+server = app.server
 
 # Build App
 #app = JupyterDash(__name__) #!!!!!
@@ -73,6 +56,7 @@ def update_figure(column):
             )
 
 # app.run_server(debug=True, mode='inline', port=8058)
+
 
 if __name__ == '__main__':
     app.run_server()
