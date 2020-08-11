4db39ede3e315df36e1a93f0a1af48373ae69c7d
Geoff <gnel.syncronetics@gmail.com>
2020-08-10 20:00:50 -0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Aug 10 18:22:54 2020
-
-@author: MasterDK-Laptop
-"""
 # import main libs
 import os
 import pandas as pd
@@ -15,57 +9,69 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output# Load Data
 
-#fileurl = 'https://github.com/Printalect/Property-Listings-Dashboard/blob/master/property-listings-downsampled.csv'
 fileurlraw = 'https://raw.githubusercontent.com/Printalect/Property-Listings-Dashboard/master/property-listings-downsampled.csv'
 rawdata = pd.read_csv(fileurlraw)
 
-# Dashboard one
-plotdata = rawdata.groupby(['state', 'type'])['price'].mean().reset_index()
-#plotdata.rename(columns={'id':'price'}, inplace=True)
+# Dashboard Two
 typesconsidered = ['house', 'townhouse', 'apartment']
+plotdata = rawdata[rawdata['type'].isin(typesconsidered)]
+plotdata = plotdata.groupby('state').filter(
+    lambda x: len(x) >= 100)  # only listings of 100 or more
+#plotdata.set_index('id',inplace=True)
+statesconsidered = sorted(list(set(plotdata['state'])))
 
 external_stylesheets1 = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-#from jupyter_dash import JupyterDash
-# app = JupyterDash(__name__, external_stylesheets=external_stylesheets1)
 app =  dash.Dash(__name__, external_stylesheets=external_stylesheets1)
 server = app.server
 
 # Build App
-#app = JupyterDash(__name__) #!!!!!
 app.layout = html.Div([
     html.H2("Dashboard: Housing"),
     dcc.Graph(id='graph'),
     html.Label([
         "Type Selection",
-        dcc.Dropdown(
-            id='column-dropdown',
-            clearable=False,
-            value=typesconsidered[2],
-            options=[{
-                'label': c.title(),
-                'value': c
-            } for c in typesconsidered])
+        dcc.Dropdown(id='column-dropdown-type',
+                     clearable=False,
+                     value=typesconsidered[2],
+                     options=[{
+                         'label': c.title(),
+                         'value': c
+                     } for c in typesconsidered]),
+    ]),
+    html.Label([
+        "State Selection",
+        dcc.Dropdown(id='column-dropdown-state',
+                     clearable=False,
+                     value=statesconsidered[0],
+                     options=[{
+                         'label': c,
+                         'value': c
+                     } for c in statesconsidered])
     ]),
 ])
 
 # Define callback to update graph
-@app.callback(Output('graph', 'figure'), [Input("column-dropdown", "value")])
-def update_figure(column):
-    return px.choropleth(plotdata[plotdata['type'] == column],
-                            locations='state',
-                            color='price',
-                            locationmode='USA-states',
-                            #hover_name='state'.title(),
-                            #hover_data=['']
-                            labels = {'price': 'Price'},
-                            title='Mean Price: {}'.format(column.title())
-                        )\
-            .update_layout(
-                geo_scope='usa',  # Plot only the USA instead of globe
-            )
+@app.callback(Output('graph', 'figure'), [
+    Input("column-dropdown-type", "value"),
+    Input("column-dropdown-state", "value")
+])
 
-# app.run_server(debug=True, mode='inline', port=8058)
-
+def update_figure(plottype, plotstate):
+    fig = px.scatter_mapbox(
+        plotdata[(plotdata['type'] == plottype)
+                 & (plotdata['state'] == plotstate)],
+        lat="lat",
+        lon="long",
+        color="price",
+        size="price",
+        mapbox_style="carto-positron",
+        hover_name='type',
+        hover_data=['type', 'price', 'sqfeet', 'baths', 'beds'],
+        #color_discrete_sequence=px.colors.qualitative.Set3,
+        #title = 'Listing Selection by State: {} & Type: {}'.format(plotstate, plotype),
+        size_max=15,
+        zoom=4.5)
+    return fig
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server()
