4d492e1eaf137f771551ae809dc557505c434e81
Geoff <gnel.syncronetics@gmail.com>
2020-08-10 17:56:21 -0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -2,6 +2,8 @@
 import pandas as pd
 import plotly.express as px
 
+import dash
+import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output# Load Data
